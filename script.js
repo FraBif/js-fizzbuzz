bfae723e6889b379d1bd05ef11b0d8de8e382814
FraBif <francescobif@hotmail.it>
2023-05-16 18:03:06 +0200

    js should work

--- script.js
+++ script.js
@@ -3,14 +3,15 @@ let str = "";
 for (let i = 0; i <= 100; i++) {
     str = str + i ;
     
+    if (i % 3 == 0 && i % 5 == 0) {
+        console.log("FizzBuzz")
+       } else if (i % 3 == 0) {
+        console.log("Fizz")
+       } else if (i % 5 == 0) {
+        console.log("Buzz")
+       }  else {console.log(i)}
   }
-   console.log(str)
+  
 
    
-   if (str % 3 == 0) {
-    console.log("Fizz")
-   } else if (str % 5 == 0) {
-    console.log("Buzz")
-   } else if (str % 3 == 0 && str % 5 == 0) {
-    console.log("FizzBuzz")
-   } else {console.log(str)}
+  
